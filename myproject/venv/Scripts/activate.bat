d886604c18890324a28a6332639eacb49af14506
AceSumaoy <sumaoyace78@gmail.com>
2025-05-08 21:51:06 +0800

    API push for Borrow and Return Management (For Checking)

--- myproject/venv/Scripts/activate.bat
+++ myproject/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LA\Desktop\Academic Works\Python Vue\Final Lab Exam\myproject\venv
+set VIRTUAL_ENV=D:\Ace Sumaoy\4th Year\ITEW-6\Library-Management-System\myproject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
